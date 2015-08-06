5e44fa6eb076ced74e588004a55ac33f95104f56
Moose <moose.techguy@gmail.com>
2015-08-05 18:49:01 -0700

    Yeoman installed

---
+++ app/scripts/app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name colorfulAsteroidsApp
+ * @description
+ * # colorfulAsteroidsApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('colorfulAsteroidsApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name colorfulAsteroidsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the colorfulAsteroidsApp
+ */
+angular.module('colorfulAsteroidsApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
