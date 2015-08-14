737902a30b9850151860f67232674475be43f601
Jamil Lawrence <jamil.lawrence@gmail.com>
2015-08-13 19:10:02 -0700

    Revert "Topics in the database are now displayed in "vote.html""

--- app.js
+++ app.js
@@ -13,23 +13,27 @@ app.use(express.static(__dirname + '/public')); //sets the root directory to pub
 // connection string for our database
 var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/postgres';
 
-app.get('/api/test', function(req, res){
-  pg.connect(connectionString, function(err, client, done){
-    var query = client.query('select text from topics');
-    var rows = [];
+pg.connect(connectionString, function(err, client, done){
+  var query = client.query('select text from topics');
+  var rows = [];
+  
+  if (err) {
+    return console.error('error running query', err);
+  }
 
-    if (err) {
-      return console.error('error running query', err);
-    }
-    query.on('row', function(row) {
+  query.on('row', function(row) {
+      //fired once for each row returned
       rows.push(row);
     });
-    query.on('end', function(result) {
-      return res.json(rows);
+  query.on('end', function(result) {
+      //fired once and only once, after the last row has been returned and after all 'row' events are emitted
+      //in this example, the 'rows' array now contains an ordered set of all the rows which we received from postgres
+      console.log(result.rowCount + ' rows were received');
     });
-  });
+
 
 });
+
 
 // Describes the port we're listening on. Go to 'localhost:3000' in browser
 var server = app.listen(3000);
