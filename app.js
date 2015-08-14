b8e369f47705004ef92135065ff4f26fb0013812
Moose <moose.techguy@gmail.com>
2015-08-13 23:45:04 -0700

    Node port update ver 0.2

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ morgan = require('morgan');
 
 var app = express();
 
+var port = process.env.PORT || 8081;
+
 //sets the root directory to public
 app.use(express.static(__dirname + '/public')); //sets the root directory to public
 
@@ -32,4 +34,4 @@ app.get('/api/test', function(req, res){
 });
 
 // Describes the port we're listening on. Go to 'localhost:3000' in browser
-var server = app.listen(3000);
+var server = app.listen(port);
